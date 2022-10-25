8a1243106420e7a5450cbcd88b1326c6089fed58
TUANPC\tranq <nolan.tran@alpaca.vn>
2022-10-25 23:26:11 +0700

    create knn

--- app_2.py
+++ app_2.py
@@ -28,33 +28,156 @@ def knn(test_point, k):
 
 
 if __name__ == '__main__':
-    genres = ['Blouses',
-              'Casual bottoms',
-              'Chemises',
-              'Dresses',
-              'Fine gauge',
-              'Intimates',
-              'Jackets',
-              'Jeans',
-              'Knits',
-              'Layering',
-              'Legwear',
-              'Lounge',
-              'Outerwear',
-              'Pants',
-              'Shorts',
-              'Skirts',
-              'Sleep',
-              'Sweaters',
-              'Swim',
-              'Trend']
+    genres = ['Accessories',
+              'After Sun Care',
+              'Aftershave',
+              'Anti-Aging',
+              'BB & CC Cream',
+              'BB & CC Creams',
+              'Bath & Body',
+              'Bath & Shower',
+              'Bath Soaks & Bubble Bath',
+              'Beauty Supplements',
+              'Blemish & Acne Treatments',
+              'Blotting Papers',
+              'Blush',
+              'Body Lotions & Body Oils',
+              'Body Mist & Hair Mist',
+              'Body Moisturizers',
+              'Body Products',
+              'Body Sprays & Deodorant',
+              'Body Sunscreen',
+              'Body Wash & Shower Gel',
+              'Bronzer',
+              'Brush Cleaners',
+              'Brush Sets',
+              'Candles',
+              'Candles & Home Scents',
+              'Cellulite & Stretch Marks',
+              'Cheek Palettes',
+              'Cleansing Brushes',
+              'Cologne',
+              'Cologne Gift Sets',
+              'Color Care',
+              'Color Correct',
+              'Concealer',
+              'Conditioner',
+              'Contour',
+              'Curling Irons',
+              'Curls & Coils',
+              'Decollete & Neck Creams',
+              'Deodorant & Antiperspirant',
+              'Deodorant for Men',
+              'Diffusers',
+              'Dry Shampoo',
+              'Exfoliators',
+              'Eye Brushes',
+              'Eye Cream',
+              'Eye Creams & Treatments',
+              'Eye Masks',
+              'Eye Palettes',
+              'Eye Primer',
+              'Eye Sets',
+              'Eyebrow',
+              'Eyelash Curlers',
+              'Eyeliner',
+              'Eyeshadow',
+              'Face Brushes',
+              'Face Masks',
+              'Face Oils',
+              'Face Primer',
+              'Face Serums',
+              'Face Sets',
+              'Face Sunscreen',
+              'Face Wash',
+              'Face Wash & Cleansers',
+              'Face Wipes',
+              'Facial Cleansing Brushes',
+              'Facial Peels',
+              'Facial Rollers',
+              'False Eyelashes',
+              'For Body',
+              'For Face',
+              'Foundation',
+              'Fragrance',
+              'Hair',
+              'Hair Accessories',
+              'Hair Brushes & Combs',
+              'Hair Dryers',
+              'Hair Masks',
+              'Hair Oil',
+              'Hair Primers',
+              'Hair Products',
+              'Hair Removal',
+              'Hair Removal & Shaving',
+              'Hair Spray',
+              'Hair Straighteners & Flat Irons',
+              'Hair Styling & Treatments',
+              'Hair Styling Products',
+              'Hair Thinning & Hair Loss',
+              'Hand Cream & Foot Cream',
+              'High Tech Tools',
+              'Highlighter',
+              'Holistic Wellness',
+              'Leave-In Conditioner',
+              'Lid Shadow Brush',
+              'Lip Balm & Treatment',
+              'Lip Balms & Treatments',
+              'Lip Brushes',
+              'Lip Gloss',
+              'Lip Liner',
+              'Lip Plumper',
+              'Lip Sets',
+              'Lip Stain',
+              'Lip Sunscreen',
+              'Lip Treatments',
+              'Lipstick',
+              'Liquid Lipstick',
+              'Lotions & Oils',
+              'Makeup',
+              'Makeup & Travel Cases',
+              'Makeup Bags & Travel Cases',
+              'Makeup Palettes',
+              'Makeup Removers',
+              'Mascara',
+              'Mini Size',
+              'Mirrors & Sharpeners',
+              'Mists & Essences',
+              'Moisturizer & Treatments',
+              'Moisturizers',
+              'Nail',
+              'Night Creams',
+              'Perfume',
+              'Perfume Gift Sets',
+              'Powder Brush',
+              'Rollerballs & Travel Size',
+              'Scalp & Hair Treatments',
+              'Scrub & Exfoliants',
+              'Self Tanners',
+              'Setting Spray & Powder',
+              'Shampoo',
+              'Shampoo & Conditioner',
+              'Shaving',
+              'Sheet Masks',
+              'Skincare',
+              'Skincare Sets',
+              'Spa Tools',
+              'Sponges & Applicators',
+              'Sunscreen',
+              'Teeth Whitening',
+              'Tinted Moisturizer',
+              'Toners',
+              'Tweezers & Eyebrow Tools',
+              'Value & Gift Sets',
+              'Wellness',
+              'no category']
 
     products = [title[0] for title in product_titles]
     st.header('Product Recommendation System')
     apps = ['--Select--', 'Product based', 'Category based']
     app_options = st.selectbox('Select application:', apps)
 
-    if app_options == 'Movie based':
+    if app_options == 'Product based':
         product_select = st.selectbox(
             'Select product:', ['--Select--'] + products)
         if product_select == '--Select--':
@@ -65,9 +188,9 @@ if __name__ == '__main__':
             genres = data[products.index(product_select)]
             test_point = genres
             table = knn(test_point, n)
-            for movie, link in table:
-                # Displays movie title with link to imdb
-                st.markdown(f"[{movie}]({link})")
+            for item, link in table:
+                # Displays item title with link to imdb
+                st.markdown(f"{item}")
     elif app_options == apps[2]:
         options = st.multiselect('Select category:', genres)
         if options:
@@ -77,9 +200,9 @@ if __name__ == '__main__':
             test_point = [1 if genre in options else 0 for genre in genres]
             test_point.append(imdb_score)
             table = knn(test_point, n)
-            for movie, link in table:
-                # Displays movie title with link to imdb
-                st.markdown(f"[{movie}]({link})")
+            for item, link in table:
+                # Displays item title with link to imdb
+                st.markdown(f"{item}")
 
         else:
             st.write("This is a simple Movie Recommender application. "
