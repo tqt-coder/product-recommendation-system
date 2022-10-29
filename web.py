0be22b6b3ed03266cf68c706201520255583f605
TUANPC\tranq <nolan.tran@alpaca.vn>
2022-10-29 20:35:37 +0700

    change template

--- web.py
+++ web.py
@@ -30,7 +30,7 @@ def recommandation(title, len):
 
 @app.route('/')
 def homePage():
-    return render_template("./login.html")
+    return render_template("./index.html")
 
 
 @app.route('/recommendation', methods=['POST'])
