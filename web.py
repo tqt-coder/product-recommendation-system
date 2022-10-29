7a0ae57a2a80de4f852d75e80e7fb1b032c14221
TUANPC\tranq <nolan.tran@alpaca.vn>
2022-10-29 17:31:22 +0700

    config

--- web.py
+++ web.py
@@ -1,8 +1,40 @@
 from flask import Flask, render_template
+from msilib.schema import tables
+import streamlit as st
+import json
+import time
+import text_base_lib as lib
 
+with open(r'titles2.json', 'r+', encoding='utf-8') as f:
+    product_titles = json.load(f)
 app = Flask(__name__)
 
 
+def recommandation(title, len):
+    index = lib.indices[title]
+    similarity_scores = list(enumerate(lib.similarity_matrix[index]))
+    sorted_similarity_scores = sorted(
+        similarity_scores, key=lambda x: x[1], reverse=True)
+    top_items_scores = sorted_similarity_scores[1:len]
+
+    top_items_indices = []
+    for i in top_items_scores:
+        top_items_indices.append(str(i[0]))
+
+    table = list()
+    for j in top_items_indices:
+        table.append([lib.data['name'][int(j)], lib.data['URL'][int(j)]])
+    return table
+
+
 @app.route('/')
-def hello():
-    return render_template('./index.html')
+def homePage():
+    table = recommandation("Colonia", 6)
+    a = ""
+    for item, link in table:
+        a = a + f"[{item}]({link})"
+    return a
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
